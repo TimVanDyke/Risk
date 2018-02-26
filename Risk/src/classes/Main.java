97cbe6e899caebf11362527a98b0eee2f19724a7
Matt <Matt@35.40.166.251>
2018-02-26 14:38:51 -0500

    i love git

--- Risk/src/classes/Main.java
+++ Risk/src/classes/Main.java
@@ -3,8 +3,17 @@ package classes;
 public class Main {
 	
 	private Main() {
-		
+		Die die1 = new Die();
+		int roll = 1;
+		int numRoll = 0;
+		while (numRoll < 100) {
+			roll = die1.Roll();
+			System.out.println("Roll " + numRoll + ": " + roll);
+			if(roll < 1 || roll > 6){
+				System.
+			}
+			numRoll++;
+		}
 	}
 }
 //hi tim
-
