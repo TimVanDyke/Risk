40bc0f906a5b5dc20030eb0189f94f911f4d4b76
Matt <Matt@35.40.255.26>
2018-03-02 07:40:24 -0500

    junits

--- Risk/src/classes/JUnit.java
+++ Risk/src/classes/JUnit.java
@@ -16,20 +16,42 @@ public class JUnit {
 	
 	@Test
 	public void testAttack(){
-		String a = "hi";
+		Player p = new Player("P1");
+		Player q = new Player("P2");
 		Board b = new Board();
 		Country att = new Country("att", 1);
 		Country def = new Country("def", 1);
 		att.setNumUnits(2);
 		def.setNumUnits(1);
+		p.addCountry(att);
+		q.addCountry(def);
 		assertTrue(att.getNumUnits() == 2 && def.getNumUnits() == 1);
-		Die attDice[] = new Die[1];
-		Die defDice[] = new Die[1];
+		Die attDice[] = new Die[3];
+		Die defDice[] = new Die[2];
+		for (int i = 0; i < attDice.length; i++)
+			attDice[i] = new Die();
+		
+		for (int i = 0; i < defDice.length; i++)
+			defDice[i] = new Die();
 		b.setAtt(attDice);
 		b.setDef(defDice);
 		assertTrue(b.getAtt() == attDice && b.getDef() == defDice);
 		b.attack(att, def, attDice, defDice);
 		assertTrue(att.getNumUnits() != 1 || def.getNumUnits() != 1);
+	}
+	
+	@Test
+	public void testPlayer(){
+		Player p = new Player("P1");
+		Country a = new Country("a", 1);
+		p.addCountry(a);
+		assertTrue(p.getKingdomSize()==1);
+		p.removeCountry(a);
+		assertTrue(p.getKingdomSize()==0);
+		assertTrue(p.isInGame() == true);
+		assertNotNull(p.getName());
+		
+		
 	}
 	
 
